ca03064a77dec9616b76794cb9f940f102ead8cc
eoeo777755@naver.com <eoeo777755@naver.com>
2022-06-17 19:03:27 +0900

    first commit

---
+++ Spring0617/src/main/java/com/sparta/spring0617/Spring0617Application.java
@@ -0,0 +1,13 @@
+package com.sparta.spring0617;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0617Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring0617Application.class, args);
+	}
+
+}
